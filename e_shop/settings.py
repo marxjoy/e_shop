2028a7d03f58ad7a988263440ab592d9b9ae7eed
Joy <jooy@interia.pl>
2019-12-11 12:01:51 +0100

    images displaying

--- e_shop/settings.py
+++ e_shop/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'https://aqueous-atoll-04338.herokuapp.com/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 LOGIN_URL = 'login'
